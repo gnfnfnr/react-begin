0c1bdf3a72d2bb07267e16df22d6758e2ae5aa18
ramda <91455804+gnfnfnr@users.noreply.github.com>
2022-01-13 01:19:46 +0900

    todolist
    1.state을 직접적으로 변경x, 함수를 사용하여 변경
    2.배열 추가하기
    1. state에는 항상 새로운 것이 추가되어야 함→따라서 배열도 새로 만들어야 함
    2. modifier에 값을 주는 방법은 함수와 고정된 값을 주는 방법이 있음
    3. 함수로 주는 방법에 첫번째 인자는 현재의 state 값임
    4. 이전의 값도 배열이겠지.
    5. 배열과 배열을 합치는 방법: [배열, ...배열]
    3. map: item을 원하는 모양으로 변환시킨 후 새로운 배열로 만들어준다.첫번째 인자는 item을 가져온다. 두번째는 index를 가져온다.
        1. Key는 React가 어떤 항목을 변경, 추가 또는 삭제할지 식별하는 것을 돕는다.
        2. key는 엘리먼트에 안정적인 고유성을 부여하기 위해 배열 내부의 엘리먼트에 지정해야 한다.

--- src/App.js
+++ src/App.js
@@ -2,34 +2,33 @@ import Button from "./Button";
 import styles from "./App.module.css";
 import { useState, useEffect } from "react";
 
-function Hello(params) {
-  // useEffect(()=>{
-  //   console.log("i am here");
-  //   return () =>(console.log("bye"));
-  // }, []);
-  function byFn(params) {
-    console.log("bye")
-  }
-  function helloFn(params) {
-    console.log("i am here");
-    return byFn;
-  }
-  useEffect(helloFn, [])
-  return(
-    <h1>Hello!</h1>
-  );
-}
-
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => {setShowing(prev => !prev)}
-  console.log("choice");
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => {
+    setToDo(event.target.value);
+  };
+  const onSubmit  = (event) =>{
+    event.preventDefault();
+    if (toDo===""){
+      return;
+    }
+    setToDos(currentArray => [toDo, ...currentArray]);
+    setToDo("");
+  };
+  console.log(toDos)
   return(
-    <div>
-      {showing? <Hello />: null}
-      <button onClick={onClick}>{showing? "Hide": "Show"}</button>
-    </div> 
-  );
+  <div>
+    <h1>My to do {(toDos.length)}</h1>
+    <form onSubmit={onSubmit}>
+      <input type="text" placeholder="Write your to do" value={toDo} onChange={onChange}/>
+      <button>Add to do</button>
+    </form>
+    <hr />
+    <ul>
+      {toDos.map((item, index)=><li key={index}>{item}</li>)}
+    </ul>
+  </div>);
 }
 
 export default App;
